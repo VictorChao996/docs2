3cf6367dfcd476be10b68da6eb80e12a06a52f21
VictorChao996 <96094victor@gmail.com>
2024-09-22 14:29:41 +0800

    update docs

---
+++ scripts/git-push.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Add all files to the staging area
+git add .
+
+# Commit the changes with a message
+git commit -m "update docs"
+
+# Push the changes to the remote repository
+git push
